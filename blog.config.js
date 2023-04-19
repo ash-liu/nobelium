f482e5fd99c8a4d745b4e58bb04b6cbe8d915a5f
ash <ww7472242@gmail.com>
2023-04-19 13:24:24 +0800

    Update blog.config.js
    modify

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'ash's blog',
+  title: 'BLOG',
   author: 'ash',
-  email: 'ww7472242@gmail.com',
-  link: 'blog.ashliu.com',
-  description: 'ash`s blog',
+  email: 'i@craigary.net',
+  link: 'https://nobelium.vercel.app',
+  description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
