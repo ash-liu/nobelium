44cc2886301c34027da8df94e7285df5fd23210f
ash <ww7472242@gmail.com>
2023-04-19 13:07:23 +0800

    Update blog.config.js
    修改相关内容

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'ash's blog',
+  author: 'ash',
+  email: 'ww7472242@gmail.com',
+  link: '',
+  description: '知行合一.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
